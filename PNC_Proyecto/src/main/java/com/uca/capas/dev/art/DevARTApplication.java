1d8e769899257844c3c07a72f765ff6c78def484
Drubico <00121117@uca.edu.sv>
2020-06-19 15:13:59 -0600

    Agregando repo vacio

---
+++ PNC_Proyecto/src/main/java/com/uca/capas/dev/art/DevARTApplication.java
@@ -0,0 +1,13 @@
+package com.uca.capas.dev.art;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevARTApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevARTApplication.class, args);
+	}
+
+}
