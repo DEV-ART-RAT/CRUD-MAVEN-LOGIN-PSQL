386b51645e90a9b8baede5c3c3353bcd33947f05
Drubico <00121117@uca.edu.sv>
2020-07-06 14:19:50 -0600

    Registro:Intento de arreglar

---
+++ PNC_Proyecto/src/main/java/devART/uca/capas/controller/CoordinadorController.java
@@ -0,0 +1,112 @@
+package devART.uca.capas.controller;
+
+import java.security.Principal;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import devART.uca.capas.domain.AppUser;
+import devART.uca.capas.domain.Expediente;
+import devART.uca.capas.domain.Materia;
+import devART.uca.capas.domain.UserRole;
+import devART.uca.capas.service.AppRoleService;
+import devART.uca.capas.service.AppUserService;
+import devART.uca.capas.service.ExpedienteServiceImpl;
+import devART.uca.capas.service.MateriaService;
+import devART.uca.capas.service.UserDetailsServiceImpl;
+import devART.uca.capas.service.UserRoleService;
+import devART.uca.capas.utils.EncrytedPasswordUtils;
+import devART.uca.capas.utils.WebUtils;
+ 
+@Controller
+public class CoordinadorController {
+	@Autowired
+	ExpedienteServiceImpl expedienteService;
+    //cordinador
+    @RequestMapping(value = { "/coordi" }, method = RequestMethod.GET)
+    public String coordinadorpage(Model model) {
+        model.addAttribute("title", "Welcome");
+        model.addAttribute("message", "This is welcome page!");
+        return "/Coordinador/coordinador";
+    }
+    @RequestMapping(value = { "/expediente" }, method = RequestMethod.GET)
+    public String expediente(Model model) {
+        model.addAttribute("title", "Welcome");
+        model.addAttribute("message", "This is welcome page!");
+        return "/Coordinador/buscarExpediente";
+    }
+    @RequestMapping(value = { "/editarexpediente" }, method = RequestMethod.GET)
+    public String editarexpediente(Model model) {
+        model.addAttribute("title", "Welcome");
+        model.addAttribute("message", "This is welcome page!");
+        return "/Coordinador/editarExpediente";
+    }
+    @RequestMapping(value = { "/resultadoExpediente" }, method = RequestMethod.GET)
+    public String resultadoExpediente(Model model) {
+        model.addAttribute("title", "Welcome");
+        model.addAttribute("message", "This is welcome page!");
+        return "/Coordinador/resultadobusquedaExpediente";
+    }
+    @RequestMapping(value = { "/macursa" }, method = RequestMethod.GET)
+    public String materiascursadas(Model model) {
+        model.addAttribute("title", "Welcome");
+        model.addAttribute("message", "This is welcome page!");
+        return "MateriasCursadas";
+    }
+    
+    @RequestMapping("/guardarExpediente")
+	public ModelAndView guardarExpediente(@Valid @ModelAttribute Expediente expediente, BindingResult result) {
+		
+		ModelAndView mav = new ModelAndView();
+		if(!result.hasErrors()) {
+			try {
+				DateTimeFormatter fmt = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+				LocalDate ahora = LocalDate.now();
+				LocalDate fechaNac = LocalDate.parse(expediente.getD_fnacimiento(), fmt);
+				System.out.println("Fecha NAch es  "+fechaNac);
+				System.out.println("getD_fnacimiento() es  "+expediente.getD_fnacimiento());
+				Period periodo = Period.between(fechaNac, ahora);
+				expediente.setS_edad(Integer.toString(periodo.getYears()));
+				expedienteService.insert(expediente);
+				
+			}catch (Exception e) {
+				e.printStackTrace();
+			}
+			
+			expediente = new Expediente();
+			mav.addObject("expediente", expediente); 
+			mav.addObject("message", "Estudiante Guardado!");
+		}
+
+		mav.setViewName("/Coordinador/AgregarExpediente");
+		return mav;
+	}
+
+    @RequestMapping("/NuevoExpediente")
+    public ModelAndView NuevoExpediente() {
+    	ModelAndView mav = new ModelAndView();
+    	Expediente expediente=new Expediente();
+    	mav.addObject("expediente", expediente);
+		mav.setViewName("/Coordinador/AgregarExpediente");
+        return mav;
+    }
+ 
+}
+
+
+//https://stackoverflow.com/questions/24802681/org-springframework-validation-beanpropertybindingresult-exception
