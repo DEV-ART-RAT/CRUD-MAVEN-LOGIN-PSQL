224b64288fc126756b52b2e27b385c1c86bfff82
CristianAlfaro <00167917@uca.edu.sv>
2020-07-06 21:40:09 -0600

    fix repository

--- PNC_Proyecto/src/main/java/devART/uca/capas/controller/UserController.java
+++ PNC_Proyecto/src/main/java/devART/uca/capas/controller/UserController.java
@@ -171,6 +171,89 @@ public class UserController {
 		return mav;
 
    	}
+
+    @RequestMapping("/ingresarMateria")
+	public ModelAndView ingresarMateria() {
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("materia", new Materia());
+		mav.setViewName("/Administrador/ingresarMateria");
+		return mav;
+	}
+
+	@PostMapping("/nuevaMateria")
+	public ModelAndView newMateria(@Valid @ModelAttribute Materia materia, BindingResult result) {
+
+		ModelAndView mav = new ModelAndView();
+		if(!result.hasErrors()) {
+			try {
+				materiaService.insert(materia);
+
+			}catch (Exception e) {
+				e.printStackTrace();
+			}
+
+			materia = new Materia();
+			mav.addObject("materia", materia);
+
+		}
+		mav.setViewName("/Administrador/ingresarMateria");
+		return mav;
+	}
+
+	@RequestMapping(value = "/materiasLista", method = RequestMethod.GET)
+	public ModelAndView listadoMateria() {
+		ModelAndView mav = new ModelAndView();
+		List<Materia> materias = null;
+		try {
+			materias = materiaService.findAll();
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		mav.addObject("materias", materias);
+		mav.setViewName("/Administrador/editorMaterias");
+
+		return mav;
+	}
+
+	@RequestMapping(value="/editarMateria")
+	public ModelAndView editar(@RequestParam(value="id") String codigo)
+	{
+		ModelAndView mav = new ModelAndView();
+		Materia materia=null;
+		try {
+			materia = materiaService.findOne(codigo);
+			mav.addObject("materia", materia);
+			mav.setViewName("/Administrador/modificarMateria");
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		return mav;
+	}
+
+	@RequestMapping("/updateMateria")
+	public ModelAndView guardarExpedientemodificado(@Valid @ModelAttribute Materia materia, BindingResult result) {
+
+		ModelAndView mav = new ModelAndView();
+		List<Materia> materias = null;
+		if(!result.hasErrors()) {
+			try {
+				materiaService.insert(materia);
+				materias = materiaService.findAll();
+
+			}catch (Exception e) {
+				e.printStackTrace();
+			}
+
+			materia = new Materia();
+			mav.addObject("materias", materias);
+			mav.addObject("message", "Materia Modificada!");
+		}
+
+		mav.setViewName("/Administrador/editorMaterias");
+		return mav;
+	}
+
+
 }
 
 
