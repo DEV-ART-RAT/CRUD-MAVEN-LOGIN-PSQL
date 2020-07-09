d26e0402b2b7f939afc833b350c795d3be6c97fb
luist23 <00127717@uca.edu.sv>
2020-07-09 14:05:39 -0600

    instituciones manager add

--- PNC_Proyecto/src/main/java/devART/uca/capas/controller/InstitucionController.java
+++ PNC_Proyecto/src/main/java/devART/uca/capas/controller/InstitucionController.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -29,6 +30,42 @@ public class InstitucionController {
 
     @Autowired
     MunicipioService municipioService;
+
+
+    @RequestMapping(value="/editarInstitucion")
+    public ModelAndView editar(@RequestParam(value="id") Integer codigo)
+    {
+        ModelAndView mav = new ModelAndView();
+        Institucion institucion=null;
+        List<Dpto> dptos = null;
+        List<Municipio> municipios=null;
+        try {
+            institucion = institucionRepository.findById(codigo).get();
+            mav.addObject("institucion", institucion);
+            mav.setViewName("/Administrador/modificarInstitucion");
+            dptos = dptoService.findAll();
+            municipios = municipioService.findAll();
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+        mav.addObject("dptos", dptos);
+        mav.addObject("municipios",municipios);
+        return mav;
+    }
+
+    @RequestMapping(value = "/institucionLista", method = RequestMethod.GET)
+    public ModelAndView listadoInstitucion() {
+        ModelAndView mav = new ModelAndView();
+        List<Institucion> instituciones = null;
+        try {
+            instituciones = institucionRepository.findAll();
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+        mav.addObject("instituciones", instituciones);
+        mav.setViewName("/Administrador/editorInstituciones");
+        return mav;
+    }
 
     @RequestMapping("/ingresarInstitucion")
     public ModelAndView ingresarMateria() {
@@ -50,7 +87,7 @@ public class InstitucionController {
     }
 
     @RequestMapping("/validarIngresarInstitucion")
-    public ModelAndView ingresarUsuarioVerificar(@ModelAttribute("institucion") @Valid Institucion institucion, BindingResult result) {
+    public ModelAndView ingresarUsuarioVerificar(@RequestParam(value="codigo") Integer codigo,@ModelAttribute("institucion") @Valid Institucion institucion, BindingResult result) {
         ModelAndView mav = new ModelAndView();
         //System.out.println(usery.getUserName() + userExp.getDpto().getNombre());
 
@@ -74,8 +111,15 @@ public class InstitucionController {
             mav.addObject("institucion", new Institucion());
             mav.addObject("message", "Instituion Guardada!");
         }
+        if(codigo.equals("")||codigo.equals(0)){
+            mav.setViewName("/Administrador/ingresarInstitucion");
+        }else {
+            mav.setViewName("/Administrador/modificarInstitucion");
+            mav.addObject("id",codigo);
+            mav.addObject("institucion", institucion);
+        }
 
-        mav.setViewName("/Administrador/ingresarInstitucion");
+
         return mav;
     }
 
